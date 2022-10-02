b060191f82a10e260ec94c2cb8b385bf80af0a20
Daniel Richter <danielrichter@posteo.de>
2022-10-02 18:07:37 +0200

    update to new bumpup
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
-const version = core.getInput('version', {required: false}) || '1.0.0-15';
+const version = core.getInput('version', {required: false}) || 'main';
 const args = core.getInput('args', {required: false}) || '';
 const cwd = core.getInput('cwd', {required: false}) || '.';
 
 (async () => {
     try {
-        await exec.exec(`deno install -n bumpup --root . --allow-all https://raw.githubusercontent.com/danielr1996/bumpup/${version}/packages/cli/mod.ts`)
+        await exec.exec(`deno install -n bumpup --root . --allow-all https://raw.githubusercontent.com/bumpupapp/cli/${version}/mod.ts`)
         core.addPath(process.cwd()+'/bin');
     } catch (error) {
         core.setFailed(error.message);
