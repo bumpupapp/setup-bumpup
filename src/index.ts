c16659340b444f55b342b34b7586c1d5225ce779
Daniel Richter <danielrichter@posteo.de>
2022-12-14 11:49:07 +0100

    build
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as tc from '@actions/tool-cache'
+
+const version = core.getInput('version', {required: false}) || '';
+
+try {
+    let toolPath = tc.find("bumpup", version)
+    if (!toolPath) {
+        const baseUrl = `https://packages.danielr1996.de/@bumpup/cli${version !== '' ? `@` : ''}${version}`
+        let platformUrl
+        if (process.platform === 'win32') {
+            platformUrl = '/bumpup.exe'
+        } else if (process.platform === 'linux') {
+            platformUrl = '/bumpup_linux_x68'
+        } else if (process.platform === 'darwin' && process.arch === 'x64') {
+            platformUrl = '/bumpup_darwin_x86'
+        } else if (process.platform === 'darwin' && process.arch === 'arm64') {
+            platformUrl = '/bumpup_darwin_aarch64'
+        }
+        const bumpupPath = await tc.downloadTool(baseUrl + platformUrl)
+        console.log(bumpupPath)
+        await exec.exec("chmod", ["+x", bumpupPath], {silent: true})
+        toolPath = await tc.cacheFile(
+            bumpupPath,
+            "bumpup",
+            "bumpup",
+            version
+        )
+    }
+    console.log(toolPath)
+    core.addPath(toolPath)
+} catch (error) {
+    core.setFailed(error.message);
+}
